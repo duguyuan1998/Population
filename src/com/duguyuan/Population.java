4178a6f519af63223aec10a69950bf7eed43ef1c
duguyuan1998 <1820556468@qq.com>
2021-01-27 12:49:16 +0800

    Population code first commit

---
+++ src/com/duguyuan/Population.java
@@ -0,0 +1,12 @@
+package com.duguyuan;
+
+public class Population {
+
+    public static void main(String[] args) {
+        long worldPopulation = 7100000000L; // 世界人口
+        int chinesePopulation = 1400000000; // 中国人口
+        System.out.println("2019年世界人口总数约：" + worldPopulation);
+        System.out.println("2019年中国人口总数约：" + chinesePopulation);
+    }
+
+}
